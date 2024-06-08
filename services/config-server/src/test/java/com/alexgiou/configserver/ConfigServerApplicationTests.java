a0c0114a3f00c85d2a7f0b27ee648ff83c970a3f
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-06-08 14:10:52 +0300

    config-server

---
+++ services/config-server/src/test/java/com/alexgiou/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexgiou.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
