daed99abe7e330028c07b82bd00975a18abea0b7
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-06-15 17:45:17 +0300

    controller.

---
+++ services/payment/src/main/java/com/alexgiou/ecommerce/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.alexgiou.ecommerce.controller;
+
+import com.alexgiou.ecommerce.payment.PaymentRequest;
+import com.alexgiou.ecommerce.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createPayment(@RequestBody @Valid PaymentRequest paymentRequest) {
+        return ResponseEntity.ok(paymentService.createPayment(paymentRequest));
+    }
+}
