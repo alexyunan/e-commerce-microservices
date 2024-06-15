f6d7e195940f7a9a612ee7de2bf646f38edc774e
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-06-15 13:52:08 +0300

    order entities.

---
+++ services/order/src/main/java/com/alexgiou/ecommerce/orderline/OrderLine.java
@@ -0,0 +1,24 @@
+package com.alexgiou.ecommerce.orderline;
+
+import com.alexgiou.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private  Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+    private double quantity;
+}
