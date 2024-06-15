723c09be6195d4db9b6e6e2e5aeacf1bca7953cc
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-06-15 16:38:45 +0300

    order confirmation message to the 'order-topic' Kafka topic.

---
+++ services/order/src/main/java/com/alexgiou/ecommerce/kafka/OrderProducer.java
@@ -0,0 +1,34 @@
+package com.alexgiou.ecommerce.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderProducer {
+
+    private final KafkaTemplate<String, OrderConfirmation> kafkaTemplate;
+
+
+    /**
+     * This method sends an order confirmation message to the 'order-topic' Kafka topic.
+     *
+     * @param orderConfirmation The order confirmation object to be sent as a message.
+     */
+    // Send the order confirmation to the order-topic
+    public void sendOrderConfirmation(OrderConfirmation orderConfirmation) {
+        log.info("Sending order confirmation: {}", orderConfirmation);
+        Message<OrderConfirmation> message = MessageBuilder
+                .withPayload(orderConfirmation)
+                .setHeader(KafkaHeaders.TOPIC, "order-topic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
