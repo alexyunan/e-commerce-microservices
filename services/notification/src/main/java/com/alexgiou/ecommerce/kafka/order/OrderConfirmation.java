6db170023fbbe915f68383037d046e06f26b31b8
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-06-19 14:33:30 +0300

    added order fields.

--- services/notification/src/main/java/com/alexgiou/ecommerce/kafka/order/OrderConfirmation.java
+++ services/notification/src/main/java/com/alexgiou/ecommerce/kafka/order/OrderConfirmation.java
@@ -1,4 +1,15 @@
 package com.alexgiou.ecommerce.kafka.order;
 
-public record OrderConfirmation() {
+import com.alexgiou.ecommerce.kafka.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        Customer customer,
+        List<Product> products
+) {
 }
